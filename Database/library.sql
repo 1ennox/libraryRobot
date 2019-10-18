3aa075f888fbde361a6e16f3c8080a729844980a
lennox805 <774146430@qq.com>
2019-10-18 21:01:19 +0800

    add compareLaercode DB  table

--- Database/library.sql
+++ Database/library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： 127.0.0.1
--- 生成日期： 2019-10-18 14:40:20
+-- 生成日期： 2019-10-18 14:59:40
 -- 服务器版本： 10.1.39-MariaDB
 -- PHP 版本： 7.1.29
 
@@ -35,23 +35,6 @@ CREATE TABLE `book` (
   `isbn` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `book`
---
-
-INSERT INTO `book` (`barCode`, `title`, `callNo`, `isbn`) VALUES
-('A000050461', ' Suetonius /', 'LOEB/S84', '0674995651'),
-('A000050502', ' Suetonius /', 'LOEB/S84', '0674995708'),
-('A000050733', ' The elder : declamations /', 'LOEB/S46O', '9780674995116'),
-('A000050734', ' The elder : declamations /', 'LOEB/S46O', '0674995104'),
-('A000050831', ' Punica /', 'LOEB/S55', '0674993063'),
-('A000050832', ' Punica /', 'LOEB/S55', '0674993055'),
-('A000050860', ' Sidonius /', 'LOEB/S53', '0674994620'),
-('A000050861', ' Sidonius /', 'LOEB/S53', '0674993276'),
-('A000050936', ' Statius /', 'LOEB/S73', '0674012097'),
-('A000050937', ' Statius /', 'LOEB/S73', '0674012097'),
-('A000050938', ' Statius /', 'LOEB/S73', '0674012089');
-
 -- --------------------------------------------------------
 
 --
@@ -62,22 +45,6 @@ CREATE TABLE `bookread` (
   `barCode` varchar(128) NOT NULL,
   `layerCode` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 转存表中的数据 `bookread`
---
-
-INSERT INTO `bookread` (`barCode`, `layerCode`) VALUES
-('A000050733', '01020202100701'),
-('A000050734', '01020202100701'),
-('A000050831', '01020202100701'),
-('A000050832', '01020202100701'),
-('A000050842', '01020202100601'),
-('A000050860', '01020202100701'),
-('A000050861', '01020202100701'),
-('A000050936', '01020202100701'),
-('A000050937', '01020202100701'),
-('A000050938', '01020202100701');
 
 -- --------------------------------------------------------
 
@@ -90,13 +57,16 @@ CREATE TABLE `comparebarcode` (
   `title` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- 转存表中的数据 `comparebarcode`
+-- 表的结构 `comparelayercode`
 --
 
-INSERT INTO `comparebarcode` (`barCode`, `title`) VALUES
-('A000050461', ' Suetonius /'),
-('A000050502', ' Suetonius /');
+CREATE TABLE `comparelayercode` (
+  `barCode` varchar(36) NOT NULL,
+  `title` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -113,13 +83,6 @@ CREATE TABLE `layer` (
   `Tier` int(36) NOT NULL,
   `LayerCode` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 转存表中的数据 `layer`
---
-
-INSERT INTO `layer` (`LibraryCode`, `Level`, `RoomNumber`, `shelf`, `ColumnNumber`, `Tier`, `LayerCode`) VALUES
-(19, 2, 2, 21, 7, 1, '01020202100701');
 
 --
 -- 转储表的索引
@@ -141,6 +104,12 @@ ALTER TABLE `bookread`
 -- 表的索引 `comparebarcode`
 --
 ALTER TABLE `comparebarcode`
+  ADD PRIMARY KEY (`barCode`);
+
+--
+-- 表的索引 `comparelayercode`
+--
+ALTER TABLE `comparelayercode`
   ADD PRIMARY KEY (`barCode`);
 
 --
